592e254cdacf2ed8c6f8fc9cdd271cffaf0d5a23
ashitoshsable <ashitoshsable09@gmail.com>
2024-06-28 16:25:54 +0530

    mongodb string

--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://ashitoshsable09:9vc1QiVkes6KOXf6@ashitoshdb.gvstg6c.mongodb.net/?retryWrites=true&w=majority&appName=ashitoshdb/users");
+mongoose.connect("mongodb+srv://ashitoshsable09:*******@ashitoshdb.gvstg6c.mongodb.net/?retryWrites=true&w=majority&appName=ashitoshdb/users");
 
 const userSchema = mongoose.Schema({
     name : String,
